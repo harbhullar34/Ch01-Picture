e91d478630215ac099add27dc99f77f5b84bb392
Harpreet Kaur <harbhullar34@gmail.com>
2026-09-22 22:48:50 -0400

    Nachos size and color update
    Added Colors and Sizes to my nacho bowl and nachos
    Added one more nacho as well called nacho7

--- Picture.java
+++ Picture.java
@@ -17,6 +17,7 @@ public class Picture
     private Triangle nacho4;
     private Triangle nacho5;
     private Triangle nacho6;
+    private Triangle nacho7;
     private Circle salsa;
     private Circle salsaBowl;
     private Circle bowl;
@@ -33,6 +34,7 @@ public class Picture
         nacho4 = new Triangle(); 
         nacho5 = new Triangle(); 
         nacho6 = new Triangle(); 
+        nacho7 = new Triangle();
         salsa = new Circle(); 
         salsaBowl = new Circle();
         bowl = new Circle();
@@ -58,47 +60,52 @@ public class Picture
             salsa.makeVisible();
             
             bowl.changeColor("black");
-            bowl.changeSize(100);
+            bowl.changeSize(200);
             bowl.moveHorizontal(-10);
-            bowl.moveVertical(40);
-            bowl.changeSize(50);
+            bowl.moveVertical(-40);
             bowl.makeVisible();
             
             nacho1.changeColor("yellow");
             nacho1.changeSize(50, 50);
-            nacho1.moveHorizontal(40);
+            nacho1.moveHorizontal(80);
             nacho1.moveVertical(40);
             nacho1.makeVisible();
             
             nacho2.changeColor("yellow");
-            nacho2.changeSize(50, 50);
-            nacho2.moveHorizontal(20);
-            nacho2.moveVertical(-60);
+            nacho2.changeSize(50, 40);
+            nacho2.moveHorizontal(150);
+            nacho2.moveVertical(30);
             nacho2.makeVisible();
             
             nacho3.changeColor("yellow");
-            nacho3.changeSize(50, 50);
-            nacho3.moveHorizontal(20);
-            nacho3.moveVertical(-60);
+            nacho3.changeSize(50, 40);
+            nacho3.moveHorizontal(80);
+            nacho3.moveVertical(-20);
             nacho3.makeVisible();
             
             nacho4.changeColor("yellow");
-            nacho4.changeSize(50, 50);
-            nacho4.moveHorizontal(20);
-            nacho4.moveVertical(-60);
+            nacho4.changeSize(50, 65);
+            nacho4.moveHorizontal(70);
+            nacho4.moveVertical(-75);
             nacho4.makeVisible();
         
             nacho5.changeColor("yellow");
-            nacho5.changeSize(50, 50);
-            nacho5.moveHorizontal(20);
-            nacho5.moveVertical(-60);
+            nacho5.changeSize(40, 30);
+            nacho5.moveHorizontal(170);
+            nacho5.moveVertical(-30);
             nacho5.makeVisible();
             
             nacho6.changeColor("yellow");
-            nacho6.changeSize(50, 50);
-            nacho6.moveHorizontal(20);
-            nacho6.moveVertical(-60);
+            nacho6.changeSize(50, 30);
+            nacho6.moveHorizontal(130);
+            nacho6.moveVertical(-20);
             nacho6.makeVisible();
+            
+            nacho7.changeColor("yellow");
+            nacho7.changeSize(40, 30);
+            nacho7.moveHorizontal(130);
+            nacho7.moveVertical(-70);
+            nacho7.makeVisible();
            
             drawn = true;
         }
@@ -115,6 +122,7 @@ public class Picture
         nacho4.changeColor("white");
         nacho5.changeColor("white");
         nacho6.changeColor("white");
+        nacho7.changeColor("white");
         salsa.changeColor("grey");
         salsaBowl.changeColor("black");
         bowl.changeColor("black");
@@ -131,6 +139,7 @@ public class Picture
         nacho4.changeColor("yellow");
         nacho5.changeColor("yellow");
         nacho6.changeColor("yellow");
+        nacho7.changeColor("yellow");
         salsaBowl.changeColor("black");
         salsa.changeColor("red");
         bowl.changeColor("black");
